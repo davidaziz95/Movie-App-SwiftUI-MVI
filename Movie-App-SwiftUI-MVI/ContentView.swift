cd1d86119c223fff5f10ee19be5a9aff87099155
david.aziz <david.aziz@evapharma.com>
2022-09-03 02:45:20 +0200

    Initial Commit

---
+++ Movie-App-SwiftUI-MVI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movie-App-SwiftUI-MVI
+//
+//  Created by David Aziz [Pharma] on 03/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Movie-App-SwiftUI-MVI/Movie_App_SwiftUI_MVIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_App_SwiftUI_MVIApp.swift
+//  Movie-App-SwiftUI-MVI
+//
+//  Created by David Aziz [Pharma] on 03/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Movie_App_SwiftUI_MVIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
